eccdcf12c8e5836d949b6758fa1dfaaaf056f358
minfield <reFugd?3e@gmail.com>
2018-04-27 00:42:02 +0800

    init

---
+++ pages/components/fake-modal/fake-modal.js
@@ -0,0 +1,44 @@
+// pages/components/fake-modal/fake-modal.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    isShow: {
+      type: Boolean,
+      value: false
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    closeInMask(e) {
+      console.log(e)
+      if (e.currentTarget.id == e.target.id) {
+        this.closeModal()
+      }
+    },
+
+    closeModal() {
+      console.log('hide')
+      this.setData({
+        isShow: false
+      })
+    }, 
+
+    showModal() {
+      this.setData({
+        isShow: true
+      })
+    }
+  }
+})
---
+++ utils/util.js
@@ -0,0 +1,39 @@
+const formatTime = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+const formatNumber = n => {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+/** 
+ * 获取用户授权, 回调相应函数
+ */
+const auth =  (authName, ab) => {
+  wx.getSetting({
+    success: (res) => {
+      if (!res[authName]) {
+        wx.authorize({
+          scope: authName,
+          success: () => {
+            cb()
+          }
+        })
+      }
+      cb()
+    }
+  })
+}
+
+module.exports = {
+  formatTime: formatTime,
+  auth, 
+}
